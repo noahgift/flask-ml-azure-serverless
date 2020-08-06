16060769121a9f8f1bfa34660b607d6aa4748141
Noah Gift <noah.gift@gmail.com>
2020-08-06 13:19:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home: From Azure Pipelines</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
