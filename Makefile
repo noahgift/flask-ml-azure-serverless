25d30c5c0ae6099098eccd448cac68b80f99c0cb
Noah Gift <noah.gift@gmail.com>
2020-08-06 11:41:54 -0400

    adding flask ml application

---
+++ Makefile
@@ -0,0 +1,18 @@
+setup:
+	python3 -m venv ~/.flask-ml-azure
+	#source ~/.flask-ml-azure/bin/activate
+	
+install:
+	pip install --upgrade pip &&\
+		pip install -r requirements.txt
+
+test:
+	#python -m pytest -vv --cov=myrepolib tests/*.py
+	#python -m pytest --nbval notebook.ipynb
+
+
+lint:
+	#hadolint Dockerfile #uncomment to explore linting Dockerfiles
+	pylint --disable=R,C,W1203 app.py
+
+all: install lint test
---
+++ make_predict_azure_app.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+PORT=443
+echo "Port: $PORT"
+
+# POST method predict
+curl -d '{
+   "CHAS":{
+      "0":0
+   },
+   "RM":{
+      "0":6.575
+   },
+   "TAX":{
+      "0":296.0
+   },
+   "PTRATIO":{
+      "0":15.3
+   },
+   "B":{
+      "0":396.9
+   },
+   "LSTAT":{
+      "0":4.98
+   }
+}'\
+     -H "Content-Type: application/json" \
+     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     #your application name <yourappname>goes here
